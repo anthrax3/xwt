ff894a39ba099fcddbd4c1d38de291ff85918010
Andres G. Aragoneses <knocte@gmail.com>
2012-05-14 22:14:05 +0100

    testing testing

--- Samples/Samples/ProgressBarSample.cs
+++ Samples/Samples/ProgressBarSample.cs
@@ -37,6 +37,10 @@ namespace Samples
 		ProgressBar determinateProgressBar;
 		ProgressBar indeterminateProgressBar;
 
+		DataField<string> artist = new DataField<string> ();
+		DataField<string> title = new DataField<string> ();
+		DataField<string> status = new DataField<string> ();
+
 		public ProgressBarSample ()
 		{
 			indeterminateProgressBar = new ProgressBar ();
@@ -48,6 +52,76 @@ namespace Samples
 			determinateProgressBar.Fraction = 0.0;
 			PackStart(determinateProgressBar, BoxMode.FillAndExpand);
 			timer.Start ();
+
+			var treeView = new TreeView ();
+			TreeStore store = new TreeStore (artist, title, status);
+			treeView.Columns.Add ("Artist", artist);
+			treeView.Columns.Add ("Title", title);
+			treeView.Columns.Add ("Status", title);
+			treeView.DataSource = store;
+			PackStart (treeView);
+
+//			foreach(var col in treeView.Columns) {
+//				treeView.
+//			}
+
+			var table = new Table ();
+			var artistX = new Label ("Artist X");
+			artistX.BackgroundColor = Xwt.Drawing.Colors.White;
+			var artistY = new Label ("Artist Y");
+			artistY.BackgroundColor = Xwt.Drawing.Colors.LightGray;
+			var artistZ = new Label ("Artist Z");
+			artistZ.BackgroundColor = Xwt.Drawing.Colors.White;
+
+			var titleX = new Label ("Title X");
+			titleX.BackgroundColor = Xwt.Drawing.Colors.White;
+			var titleY = new Label ("Title Y");
+			titleY.BackgroundColor = Xwt.Drawing.Colors.LightGray;
+			var titleZ = new Label ("Title Z");
+			titleZ.BackgroundColor = Xwt.Drawing.Colors.White;
+
+			var pgBarX = new ProgressBar ();
+			pgBarX.Indeterminate = true;
+			var pgBarY = new ProgressBar ();
+			pgBarY.Indeterminate = true;
+			var pgBarZ = new ProgressBar ();
+			pgBarZ.Indeterminate = true;
+
+
+			MenuButton mbA = new MenuButton ("Artist");
+			mbA.Margin.SetAll (-1);
+			table.Attach (mbA, 0, 0, AttachOptions.Fill, AttachOptions.Fill);
+
+			Button mbT = new Button ("Title");
+			mbT.Margin.SetAll (-1);
+			table.Attach (mbT, 1, 0, AttachOptions.Fill, AttachOptions.Fill);
+			mbT.Surface.Reallocate ();
+
+			Button mbS = new Button ("Status");
+			mbS.Margin.SetAll (-1);
+			table.Attach (mbS, 2, 0, AttachOptions.Fill, AttachOptions.Fill);
+
+			table.Attach (artistX, 0, 1);
+			table.Attach (titleX, 1, 1);
+			table.Attach (pgBarX, 2, 1);
+
+			table.Attach (artistY, 0, 2);
+			table.Attach (titleY, 1, 2);
+			table.Attach (pgBarY, 2, 2);
+
+			table.Attach (artistZ, 0, 3);
+			table.Attach (titleZ, 1, 3);
+			table.Attach (pgBarZ, 2, 3);
+
+			table.DefaultRowSpacing = 0;
+			table.DefaultColumnSpacing = 0;
+			PackStart (table, BoxMode.Fill);
+
+			var labelHeight = artistX.Size.Height;
+			var newSize = treeView.Size;
+
+			newSize.Height = labelHeight;
+			//treeView.Surface.SizeRequestMode = SizeRequestMode.HeightForWidth;
 		}
 
 		public void Increase (object sender, ElapsedEventArgs args)
